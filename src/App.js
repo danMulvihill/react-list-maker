d87ebed5f6015a4b41fc160525d23103534e3ab1
Dan Mulvihill <dmulvihill3@gmail.com>
2018-10-09 14:19:34 -0400

    This is a functioning list making template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,148 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.onAdd = this.onAdd.bind(this);
+    this.onRemove = this.onRemove.bind(this);
+    this.state = {
+      items: []
+    };
+  }
+
+  onAdd(item) {
+    if (!item) {
+      return 'Type an item and hit Enter';
+    } else if (this.state.items.indexOf(item) > -1) {
+      return 'This item is already on the list';
+    }
+
+    this.setState((prevState) => ({
+      items: prevState.items.concat(item)
+    }));
+  }
+  onRemove(itemToRemove) {
+    this.setState((prevState) => ({
+      items: prevState.items.filter((item) => itemToRemove !== item)
+    }));
+  }
+
+  render() {
+    const subtitle = 'Add to List';
+
+    return (
+      <div>
+        <Header subtitle={subtitle} />
+        <AddItem onAdd={this.onAdd} />
+        <Items
+          items={this.state.items}
+          onRemove={this.onRemove}
+        />
+
+      </div>
+    );
+  }
+
+  //Storaging and retrieving list from LocalStorage: 
+  componentDidMount() {
+    try {
+      const json = localStorage.getItem('items');
+      const items = JSON.parse(json);
+
+      if (items) {
+        this.setState(() => ({ items }));
+      }
+    } catch (e) {
+     
+    }
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.items.length !== this.state.items.length) {
+      const json = JSON.stringify(this.state.items);
+      localStorage.setItem('items', json);
+    }
+  }
+  componentWillUnmount() {
+    console.log('componentWillUnmount');
+  }
+
+
+}
+
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.title}</h1>
+      {props.subtitle && <h2>{props.subtitle}</h2>}
+    </div>
+  );
+};
+
+Header.defaultProps = {
+  title: 'List Maker'
+};
+
+
+const Items = (props) => {
+  return (
+    <div>
+      {props.items.length === 0 && <p>Please add an item to get started!</p>}
+      {
+        props.items.map((item) => (
+          <Item
+            key={item}
+            optionText={item}
+            onRemove={props.onRemove}
+          />
+        ))
+      }
+    </div>
+  );
+};
+
+const Item = (props) => {
+  return (
+    <div>
+      {props.optionText}
+      <button
+        onClick={(e) => {
+          props.onRemove(props.optionText);
+        }}
+      >
+        remove
+      </button>
+    </div>
+  );
+};
+
+class AddItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onAdd = this.onAdd.bind(this);
+    this.state = {
+      error: undefined
+    };
+  }
+  onAdd(e) {
+    e.preventDefault();
+
+    const listItem = e.target.elements.listItem.value.trim();
+    const error = this.props.onAdd(listItem);
+
+    this.setState(() => ({ error }));
+
+    if (!error) {
+      e.target.elements.listItem.value = '';
+    }
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        {this.state.error && <p>{this.state.error}</p>}
+        <form onSubmit={this.onAdd}>
+          <input type="text" name="listItem" />
+          <button>Add Item</button>
+        </form>
       </div>
     );
   }
